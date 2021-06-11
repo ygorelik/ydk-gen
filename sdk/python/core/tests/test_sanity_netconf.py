0ed8025bf4b4efdefdaff59177352ce66c67a191
ygorelik <yangorelik@yahoo.com>
2021-06-11 16:30:10 -0700

    Skipped some NetconfService tests due to segmentation fault

--- sdk/python/core/tests/test_sanity_netconf.py
+++ sdk/python/core/tests/test_sanity_netconf.py
@@ -14,13 +14,15 @@
 # limitations under the License.
 # ------------------------------------------------------------------
 
-"""test_sanity_netconf.py
-sanity tests for netconf
+"""
+test_sanity_netconf.py
+sanity tests for Netconf Service
 """
 from __future__ import absolute_import
 from __future__ import print_function
 
 import sys
+import platform
 import unittest
 import logging
 
@@ -36,11 +38,12 @@ from ydk.providers import CodecServiceProvider
 from ydk.ext.types import EncodingFormat
 
 from test_utils import ParametrizedTestCase
-from test_utils import get_device_info
+from test_utils import get_device_info, enable_logging, logger
 
 from ydk.types  import Filter, Config
 
-class SanityNetconf(ParametrizedTestCase):
+
+class SanityNetconf(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -50,16 +53,11 @@ class SanityNetconf(ParametrizedTestCase):
             cls.password,
             cls.port,
             cls.protocol,
-            not cls.on_demand,
+            cls.on_demand,
             cls.common_cache,
-            cls.timeout)
+        )
         cls.netconf_service = NetconfService()
-        cls.logger = logging.getLogger("ydk")
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter(("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-        handler.setFormatter(formatter)
-        cls.logger.addHandler(handler)
-        cls.logger.setLevel(logging.ERROR)
+        enable_logging(logging.ERROR)
 
     def setUp(self):
         crud = CRUDService()
@@ -102,11 +100,11 @@ class SanityNetconf(ParametrizedTestCase):
         self.assertEqual(True, op)
 
         try:
-            op = self.netconf_service.unlock(self.ncc, Datastore.running)
+            self.netconf_service.unlock(self.ncc, Datastore.running)
         except Exception as e:
             self.assertIsInstance(e, YError)
 
-        op = self.netconf_service.unlock(self.ncc, Datastore.candidate)
+        self.netconf_service.unlock(self.ncc, Datastore.candidate)
 
     def test_validate(self):
         op = self.netconf_service.validate(self.ncc, source=Datastore.candidate)
@@ -143,7 +141,6 @@ class SanityNetconf(ParametrizedTestCase):
         result = self.netconf_service.get(self.ncc, get_filter)
         self.assertEqual(runner, result)
 
-    #@unittest.skip('No message id in cancel commit payload')
     def test_confirmed_commit(self):
         runner = ysanity.Runner()
         runner.two.number = 2
@@ -206,14 +203,7 @@ class SanityNetconf(ParametrizedTestCase):
         config = self.netconf_service.get_config(self.ncc, Datastore.candidate, get_filter)
         self.assertEqual(edit_filter, config)
 
-#         codec_service = CodecService()
-#         codec_provider = CodecServiceProvider()
-#         codec_provider.encoding = EncodingFormat.XML
-#         for entity in config:
-#             xml_encode = codec_service.encode(codec_provider, entity)
-#             print(xml_encode)
-
-        op = self.netconf_service.discard_changes(self.ncc)
+        self.netconf_service.discard_changes(self.ncc)
 
     def test_edit_get_config_collection(self):
         runner = ysanity.Runner()
@@ -233,7 +223,7 @@ class SanityNetconf(ParametrizedTestCase):
         config = self.netconf_service.get_config(self.ncc, Datastore.candidate, get_filter)
         self.assertEqual(edit_config, config)
 
-        op = self.netconf_service.discard_changes(self.ncc)
+        self.netconf_service.discard_changes(self.ncc)
 
     def test_delete_config(self):
         pass
@@ -291,7 +281,7 @@ class SanityNetconf(ParametrizedTestCase):
 
     @unittest.skip('TODO: if-features are not enabled')
     def test_sanity_crud_read_interface(self):
-        address = ysanity.Native.Interface.Loopback.Ipv4.Address();
+        address = ysanity.Native.Interface.Loopback.Ipv4.Address()
         address.ip = "2.2.2.2"
         address.netmask = "255.255.255.255"
 
@@ -333,7 +323,7 @@ class SanityNetconf(ParametrizedTestCase):
         bgp.global_.config.as_ = 65001
         bgp.global_.config.router_id = "1.2.3.4"
 
-        create_list = [native, bgp];
+        create_list = [native, bgp]
 
         # Configure device
         result = crud.create(self.ncc, create_list)
@@ -342,7 +332,7 @@ class SanityNetconf(ParametrizedTestCase):
         # Read configuration
         native_filter = ysanity.Native()
         bgp_filter = openconfig.Bgp()
-        filter_list = [native_filter, bgp_filter];
+        filter_list = [native_filter, bgp_filter]
 
         read_list = crud.read(self.ncc, filter_list)
         self.assertEqual(isinstance(read_list, list), True)
@@ -357,7 +347,7 @@ class SanityNetconf(ParametrizedTestCase):
         crud = CRUDService()
 
         # Build configuration of multiple objects
-        create_list = Config();
+        create_list = Config()
 
         native = ysanity.Native()
         native.hostname = 'NativeHost'
@@ -376,7 +366,7 @@ class SanityNetconf(ParametrizedTestCase):
         self.assertEqual(result, True)
 
         # Read configuration
-        read_filter = Filter([ysanity.Native(), openconfig.Bgp()]);
+        read_filter = Filter([ysanity.Native(), openconfig.Bgp()])
         read_config = crud.read(self.ncc, read_filter)
         self.assertEqual(isinstance(read_config, Config), True)
         self.assertEqual(len(read_config), 2)
@@ -394,8 +384,8 @@ class SanityNetconf(ParametrizedTestCase):
         result = crud.delete(self.ncc, create_list)
         self.assertEqual(result, True)
 
+    @unittest.skipIf('xenial' not in platform.platform(), 'Segmentation fault in libyang')
     def test_netconf_get_candidate_config(self):
-        self.logger.setLevel(logging.ERROR)
         ns = NetconfService()
         config = ns.get_config(self.ncc, Datastore.candidate)
         self.assertNotEqual(len(config), 0)
@@ -403,8 +393,8 @@ class SanityNetconf(ParametrizedTestCase):
         for entity in config:
             print(entity.path())
 
+    @unittest.skipIf('xenial' not in platform.platform(), 'Segmentation fault in libyang')
     def test_netconf_get_running_config(self):
-        self.logger.setLevel(logging.ERROR)
         ns = NetconfService()
         config = ns.get_config(self.ncc)
         self.assertNotEqual(len(config), 0)
@@ -412,8 +402,8 @@ class SanityNetconf(ParametrizedTestCase):
         for entity in config:
             print(entity.path())
 
+    @unittest.skipIf('xenial' not in platform.platform(), 'Segmentation fault in libyang')
     def test_crud_get_all_config(self):
-        self.logger.setLevel(logging.ERROR)
         crud = CRUDService()
         config = crud.read_config(self.ncc)
         self.assertNotEqual(len(config), 0)
@@ -421,8 +411,8 @@ class SanityNetconf(ParametrizedTestCase):
         for entity in config:
             print(entity.path())
 
+    @unittest.skipIf('xenial' not in platform.platform(), 'Segmentation fault in libyang')
     def test_crud_get_all(self):
-        self.logger.setLevel(logging.ERROR)
         crud = CRUDService()
         try:
             config = crud.read(self.ncc)
@@ -431,7 +421,7 @@ class SanityNetconf(ParametrizedTestCase):
             for entity in config:
                 print(entity.path())
         except YError as err:
-            self.logger.error("Failed to get device state due to error: {}".format(err.message))
+            logger.error("Failed to get device state due to error: {}".format(err.message))
 
     def test_passive_interface(self):
         ospf = ysanity.Runner.One.Ospf()
@@ -452,6 +442,7 @@ class SanityNetconf(ParametrizedTestCase):
 
         op = self.netconf_service.discard_changes(self.ncc)
         self.assertEqual(True, op)
+
 
 if __name__ == '__main__':
     device, non_demand, common_cache, timeout = get_device_info()
