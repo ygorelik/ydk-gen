14a3d7c3c0dd6babb463759f7a49150e93c74b9a
ygorelik <ygorelik@cisco.com>
2022-04-06 12:40:09 -0700

    Excluded TCP tests in docker image

--- test/tests.sh
+++ test/tests.sh
@@ -616,9 +616,9 @@ function run_py_sanity_ydktest_tests {
 
     py_sanity_ydktest_test_netconf_ssh
 
-#    if [[ ${os_info} == *"xenial"* ]]; then
-#      py_sanity_ydktest_test_tcp	# This test fails in docker
-#    fi
+    if [ ! [ -f /.dockerenv ] ]; then
+      py_sanity_ydktest_test_tcp	# This test fails in docker
+    fi
 
     stop_tcp_server
 }
